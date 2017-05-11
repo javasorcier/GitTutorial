feb46ef55759850377fa6283c71b279620cd04ae
gbin <gbin@10.0.1.3>
2017-05-11 15:38:11 +0200

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Bin G on 2017/5/11.
+//  Copyright © 2017年 Bin G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
